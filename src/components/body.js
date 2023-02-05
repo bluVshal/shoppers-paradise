ad1fa23b59f6bef2ecfc3b4a83866262ecb40cc7
bluVshal <blu.vishal@gmail.com>
2023-02-05 23:35:13 +0400

    Added sample semantic ui button

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,9 @@
+import { Button } from 'semantic-ui-react'
+
 function Body(){
     return(
         <div>
+            <Button inverted color='brown'>Sample Button</Button>
             <b> BODY </b>
         </div>
     );
